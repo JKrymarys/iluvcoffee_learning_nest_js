e8d239b23493d232d7dd1b542a9b59e241127a23
Jakub Krymarys <jakub.krymarys@stxnext.pl>
2022-01-09 11:45:22 +0100

    Playling around with service and exceptions

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -7,38 +7,44 @@ import {
   Patch,
   Delete,
   Query,
+  HttpException,
+  HttpStatus,
+  NotFoundException,
 } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
   @Get()
   findAll(@Query() paginationQuery) {
-    const { limit, offset } = paginationQuery;
-    return `All coffees. Limit: ${limit}, offset: ${offset}`;
-  }
-
-  @Get('flavours')
-  findAllFlavours() {
-    return 'All coffee flavours';
+    // const { limit, offset } = paginationQuery;
+    return this.coffeesService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This action returns #${id} coffee`;
+    const coffee = this.coffeesService.findOne(id);
+    if (!coffee) {
+      // throw new HttpException(`Coffee #${id} not found.`, HttpStatus.NOT_FOUND);
+      throw new NotFoundException(`Coffee #${id} not found.`);
+    }
+    return coffee;
   }
 
   @Post()
   create(@Body() body) {
-    return body;
+    return this.coffeesService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `This action updates #${id} coffee`;
+    return this.coffeesService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `This action removes #${id} coffee`;
+    return this.coffeesService.remove(id);
   }
 }
